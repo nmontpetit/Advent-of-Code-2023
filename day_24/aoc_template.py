4d034df86c64ca1a3d26c271e66b85867e9be709
Nicholas Montpetit <nick.montpetit@gmail.com>
2023-12-24 07:27:02 -0600

    added Day 24 parsing code

---
+++ day_24/aoc_template.py
@@ -0,0 +1,50 @@
+# aoc_template.py
+
+import pathlib
+import sys
+import re
+from typing import Iterator
+from more_itertools import grouper
+
+
+def parse(
+        puzzle_input: str
+        ) -> list[tuple[tuple[int, int, int], tuple[int, int, int]]]:
+    """Parse input"""
+    return [
+        ( tuple(int(x.strip()) for x in pos.split(',')),
+          tuple(int(x.strip()) for x in velocity.split(','))
+        )
+        for pos, velocity
+        in grouper(
+            ( val.strip()
+              for line in puzzle_input.split('\n')
+              for val in line.split('@')
+            ), 2
+        ) 
+    ]
+
+
+def part1(data):
+    """Solve part 1"""
+    pass
+
+def part2(data):
+    """Solve part 2"""
+    pass
+
+
+def solve(puzzle_input):
+    """Solve the puzzle for the given input"""
+    data = parse(puzzle_input)
+    solution1 = part1(data)
+    solution2 = part2(data)
+    return solution1, solution2
+
+
+if __name__ == "__main__":
+    for path in sys.argv[1:]:
+        print(f"{path}:")
+        puzzle_input = pathlib.Path(path).read_text().strip()
+        solutions = solve(puzzle_input)
+        print("\n".join(str(solution) for solution in solutions))
